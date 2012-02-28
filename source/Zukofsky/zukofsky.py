116049d92f7f781baaf30eb28ee71fbce0d23dd0
educruzb <educruzb@gmail.com>
2012-02-28 16:32:28 -0400

    Major changes, deprecated cmd front end. MAking the web front end

--- source/Zukofsky/zukofsky.py
+++ source/Zukofsky/zukofsky.py
@@ -12,6 +12,7 @@ import cmd
 #CONSTANTS
 VERSION = '0.1'
 AUTHOR = 'Eduardo Cruz'
+auth = tweepy.OAuthHandler("hOfmzo8cdQoRD4GYkI4XCg", "LDzfw6X38jNGA5TBmHT7LZO7iEZ2qdhMJS52cYKrE")
 #CONSTANTS
 
 
@@ -21,15 +22,17 @@ def get_version():
 def get_author():
     return AUTHOR
 
-def get_api(auth,verifier):
+def get_api(verifier):
     auth.get_access_token(verifier)
     return tweepy.API(auth)
 
 def get_url():
-    auth = tweepy.OAuthHandler("hOfmzo8cdQoRD4GYkI4XCg", "LDzfw6X38jNGA5TBmHT7LZO7iEZ2qdhMJS52cYKrE")
     return auth.get_authorization_url()
 
-def get_friends_sn(api):
+def get_me(api):
+    return api.me().screen_name
+
+def get_friends(api):
     friends = []
     for friend in tweepy.Cursor(api.friends).items():
             friends.append(friend.screen_name)
@@ -39,5 +42,5 @@ def get_followers(api):
     followers = []
     for follower in tweepy.Cursor(api.followers).items():
             followers.append(follower.screen_name)
-    return follower
+    return followers
         
